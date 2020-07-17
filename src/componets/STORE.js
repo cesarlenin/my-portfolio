856b4fc3bc4d2d53787c721528ec01f87c6afd45
Cesar Morales <cesarlenin2@live.com>
2020-07-17 10:58:15 -0700

    store was added

---
+++ src/componets/STORE.js
@@ -0,0 +1,13 @@
+const STORE = {
+    Projects : [
+        {
+            id: 1,
+            name: 'Quiz App',
+            live:'https://thinkful-ei-narwhal.github.io/Quiz-App_Cesar_Yousef/',
+            summary: "Quiz app is a game for anyone that wants to test their knowledge. The game tracks the user's current progression, as well as whether the user has selected the correct or incorrect answer. Then it will display that information after each question has been answered. At the end the user will be provided with their score and how many questions they got correct or incorrect. I made this as a tool to help me study for future tests.",
+            repo: "https://github.com/thinkful-ei-narwhal/Quiz-App_Cesar_Yousef",
+            tech: ["html","css","js","jquery"]
+        }
+    ]
+}
+export default STORE;
