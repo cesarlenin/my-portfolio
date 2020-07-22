57f198bac0042bf6cbb3b8d35de1102657a48438
Cesar Morales <cesarlenin2@live.com>
2020-07-21 20:19:25 -0700

    changed summaries for projects

--- src/componets/STORE.js
+++ src/componets/STORE.js
@@ -4,7 +4,7 @@ const STORE = {
             id: 1,
             name: 'Penny Thoughts',
             live:'https://penny-thoughts.vercel.app/',
-            summary: "This app is an uplifting, anonymous, message sharing app. Negative messages are filtered out using a toxicity filter from TensorFlow.",
+            summary: "In a world where COVID-19, boiling racial tensions, and deepening political divides are deteriorating a large portion of the population's mental health, Penny Thoughts was born from our desire to help fight off negativity. It is a space where the common folk can motivate those who fell victim to these stressful times by anonymously sharing and receiving uplifting messages.Taking into account that some users may not have the best intentions, TensorFlow AI was used as a filter to insure all messages sent are appropriate.",
             repo: "https://github.com/thinkful-ei-narwhal/penny-thoughts-client",
             tech: ["html","css","js","node","postgresql","react","knex", "mocha","express"],
             img:"/images/Penny-Thoughts.png"
@@ -13,7 +13,7 @@ const STORE = {
             id: 2,
             name: 'Space Rep',
             live:'https://spaced-rep-french.vercel.app',
-            summary: "This app helps users implement the spaced repetition technique to help users learn ten preloaded French words. New Users can create an account. Returning users can log in and begin practicing with instant feedback and reinforcement.",
+            summary: "This app was created to help anyone trying to learn French. It has implemented the spaced repetition technique to help its users quickly learn ten preloaded French words. New users can create an account, whereas returning users can log in and begin practicing with instant feedback and reinforcement.",
             repo: "https://github.com/thinkful-ei-narwhal/french-langue-cesarkevin",
             tech: ["html","css","js","node","postgresql","react","knex", "mocha","express"],
             img:"/images/Spaced-Rep.png"
@@ -22,7 +22,7 @@ const STORE = {
             id: 3,
             name: 'My Discipline- habit tracker',
             live:'https://my-discipline.now.sh/',
-            summary: "My Discipline was created to be a simple way to keep track and organize all of your habits. The user starts off by setting goals they want to meet. Then generating a collection of habits they want to have, and quickly seeing their consistency in a calendar every time they log in.",
+            summary: "My Discipline was created for individuals looking to better themselves by keeping track and organizing all of their habits. The user starts off by setting goals they want to meet. Then generating a collection of habits they want to have, and quickly seeing their consistency in a calendar every time they log in.",
             repo: "https://github.com/cesarlenin/my-discipline",
             tech: ["html","css","js","node","postgresql","react","knex", "mocha","express"],
             img:"/images/My-Discipline.png"
