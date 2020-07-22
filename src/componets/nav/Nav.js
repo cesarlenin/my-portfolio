e3cd6720e60e1f3ec0bd3aca4eef13f93495e0f1
Cesar Morales <cesarlenin2@live.com>
2020-07-22 14:33:41 -0700

    mobile nav works

--- src/componets/nav/Nav.js
+++ src/componets/nav/Nav.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 export class Nav extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          activeClassName: 'links',
+        }
+      }
+
+    closeNav() {
+        let css = (this.state.activeClassName === "links") ? "links open" : "links";
+        this.setState({activeClassName:css});
+    }
+
+
   render() {
+
     return (
         <nav>
-        <NavLink className='active home' to = '/' >Cesar Morales</NavLink> 
-        <div>
-        <NavLink className='projects' to = '/projects' >Projects</NavLink> 
-        <NavLink className='about' to = '/'>about</NavLink> 
+        <div className='navContainer'>
+        <Link className='active home' to = '/' >Cesar Morales</Link> 
+        <button className="icon" onClick={(e) => {
+          e.preventDefault();
+          this.closeNav();
+        }}>
+        <i className="fa fa-bars"></i>
+        </button>
         </div>
+        <ul id="myLinks" className={this.state.activeClassName}>
+        <li className='projects'><Link  to = '/projects' 
+        onClick={(e) => {
+          this.closeNav();
+        }}>
+          Projects
+          </Link> </li>
+        <li className='about'><Link to = '/'
+        onClick={(e) => {
+          this.closeNav();
+        }}>
+          about
+        </Link> </li>
+        </ul>
     </nav>
     )
   }
